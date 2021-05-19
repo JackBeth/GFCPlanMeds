5a276280d9db5252af9763d0b3b7a28ff5f16df2
JackBeth <jackie-raabjerg@hotmail.com>
2021-05-19 10:19:42 +0200

    fragments added etc.

--- app/src/main/java/com/example/gfcplanmeds/MainActivity.java
+++ app/src/main/java/com/example/gfcplanmeds/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.gfcplanmeds;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.NavController;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.med_nav)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.InformationFragment, R.id.MedicineFragment, R.id.AlarmFragment, R.id.DoctorFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/gfcplanmeds/Test.java
+++
@@ -1,5 +0,0 @@
-package com.example.gfcplanmeds;
-
-public class Test {
-
-}
