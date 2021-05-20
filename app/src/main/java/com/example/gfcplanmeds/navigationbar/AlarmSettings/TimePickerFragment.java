23a697eaefc0f54035f73d08fe5f409125b35d13
JackBeth <jackie-raabjerg@hotmail.com>
2021-05-20 21:48:25 +0200

    Alarm

--- app/src/main/java/com/example/gfcplanmeds/navigationbar/AlarmSettings/TimePickerFragment.java
+++ app/src/main/java/com/example/gfcplanmeds/navigationbar/AlarmSettings/TimePickerFragment.java
@@ -1,12 +1,16 @@
 package com.example.gfcplanmeds.navigationbar.AlarmSettings;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.AlarmClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.gfcplanmeds.R;
 import com.example.gfcplanmeds.data.User;
@@ -22,7 +26,21 @@ public class TimePickerFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_picker, container, false);
+        View view = inflater.inflate(R.layout.fragment_time_picker, container, false);
+        Button button = view.findViewById(R.id.SetAlarmButton);
+        EditText textHours = view.findViewById(R.id.AlarmHours);
+        EditText textMin = view.findViewById(R.id.AlarmMinutes);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent.putExtra(AlarmClock.EXTRA_HOUR, textHours.getText());
+                intent.putExtra(AlarmClock.EXTRA_MINUTES, textMin.getText());
+
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
